8ca3a5d034f0c77babbb06b4f9b54bb4d72abb0b
antonrejoe <antonrejoe@gmail.com>
2023-04-09 09:55:55 +0530

    rid of chaos

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "jotai";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import CookiesProvider from "react-cookie/cjs/CookiesProvider";
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
+
   return (
-    <CookiesProvider>
-      <Provider>
-        <Component {...pageProps} />
-      </Provider>
-    </CookiesProvider>
+    <QueryClientProvider client={queryClient}>
+      <CookiesProvider>
+        <Provider>
+          <Component {...pageProps} />
+        </Provider>
+      </CookiesProvider>
+    </QueryClientProvider>
   );
 }
--- pages/profile/[document]/index.jsx
+++ pages/profile/[document]/index.jsx
@@ -1,60 +1,121 @@
-"use client";
-
 import React, { useState, useEffect } from "react";
-import Document_playground from "../../../components/Document_playground";
 import NoSSR from "react-no-ssr";
 import { Client, Databases } from "appwrite";
+import { TextField } from "@mui/material";
 import { Cookies } from "react-cookie";
-
+import { atom, useAtom } from "jotai";
 const cookies = new Cookies();
 
 const client = new Client();
+const contentAtom = atom("");
+
+import {
+  useQueryClient,
+  QueryClient,
+  useQuery,
+  useMutation,
+} from "@tanstack/react-query";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // default: true
+    },
+  },
+});
+
 client
   .setEndpoint(process.env.NEXT_PUBLIC_END_PT)
   .setProject(process.env.NEXT_PUBLIC_PROJECT_ID);
-const db = new Databases(client);
+
+const databases = new Databases(client);
 
 export default function document() {
-  const [document, setDocument] = useState({
-    heading: "",
-    content: " ",
-    createdAt: " ",
+  const [content, setContent] = useAtom(contentAtom);
+
+  const { data, isLoading, refetch, error } = useQuery({
+    queryKey: ["getDoc"],
+    queryFn: () =>
+      databases
+        .getDocument(
+          process.env.NEXT_PUBLIC_PRIMARY_DB_ID,
+          process.env.NEXT_PUBLIC_SECONDARY_COLLECTION_ID,
+          cookies.get("id")
+        )
+        .then((res) => {
+          console.log(res);
+          return res;
+        }),
   });
-  useEffect(() => {
-    const db_sel = db.getDocument(
-      process.env.NEXT_PUBLIC_PRIMARY_DB_ID,
-      process.env.NEXT_PUBLIC_SECONDARY_COLLECTION_ID,
-      cookies.get("id")
-    );
 
-    db_sel.then(
-      (res) => {
-        console.log(" here is the doc", res);
-        setDocument({
-          ...document,
-          heading: res.heading,
-          content: res.content,
-          createdAt: res.$createdAt,
-        });
-      },
-      (err) => {
-        console.log(err);
-      }
-    );
-  }, []);
+  if (isLoading) {
+    console.log("It's loading");
+  }
 
-  return (
-    <>
-      <NoSSR>
-        <main className="">
-          <div className=" grid m-0 w-screen bg-gray-900  grid-flow-col place-items-end text-center grid-cols-1 p-3"></div>
-          <Document_playground
-            content={document.content}
-            heading={document.heading}
-            createdAt={document.createdAt}
-          ></Document_playground>
-        </main>
-      </NoSSR>
-    </>
-  );
+  const {
+    data: updateData,
+    error: updateError,
+    isLoading: updateIsLoading,
+    refetch: updateRefetch,
+  } = useQuery({
+    queryKey: ["docUpdate"],
+    exact: true,
+    queryFn: async () =>
+      databases
+        .updateDocument(
+          process.env.NEXT_PUBLIC_PRIMARY_DB_ID,
+          process.env.NEXT_PUBLIC_SECONDARY_COLLECTION_ID,
+          cookies.get("id"),
+          { content: content }
+        )
+        .then((res) => {
+          console.log(res);
+          return res;
+        }),
+  });
+
+  // const {
+  //   data: dataUpdate,
+  //   error: errorUpdate,
+  //   isLoading: updatingIsLoading,
+  //   refetch: refetchUpdate,
+  // } = useQuery({
+  //   queryKey: ["update_doc"],
+  //   queryFn: () =>
+
+  if (!!data && !!updateData) {
+    console.log(`The data is ${data.content}`);
+
+    if (!!content) {
+    }
+    return (
+      <>
+        <NoSSR>
+          <main className="">
+            <div className=" grid m-0 w-screen bg-gray-900  grid-flow-col place-items-end text-center grid-cols-1 p-3"></div>
+            <main className="w-[65ch]  m m-auto h-screen">
+              <h1 className="text-4xl my-[3rem]"> {data.heading}</h1>
+              <h4>{data.createdAt} </h4>
+              <div>
+                <TextField
+                  id="outlined-multiline-flexible"
+                  placeholder=""
+                  multiline
+                  variant="standard"
+                  sx={{
+                    width: "65ch",
+                  }}
+                  // defaultValue={data.content}
+                  defaultValue={data.content}
+                  onChange={(e) => {
+                    setContent(e.target.value);
+                  }}
+                />
+                <h1>{data.content}</h1>
+              </div>
+            </main>
+          </main>
+        </NoSSR>
+      </>
+    );
+  }
 }
